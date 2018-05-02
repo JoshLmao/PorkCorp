524489762ec871cb11c101dfc42721b7cb7408f7
Josh Shepherd <josh_lmao@live.co.uk>
2018-05-02 11:37:59 +0100

    Pigs can walk to target using NavMesh

---
+++ Assets/scripts/game/constants/TagConstants.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TagConstants
+{
+    public static string HOUSE_TRIGGER_TAG = "HouseTrigger";
+}
--- Assets/scripts/game/controllers/PigController.cs
+++ Assets/scripts/game/controllers/PigController.cs
@@ -1,21 +1,47 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class PigController : MonoBehaviour
 {
     public Transform TargetTransform { get; private set; }
 
-    void Start ()
+    public float Speed { get; set; }
+
+    NavMeshAgent m_agent;
+
+    private void Start ()
     {
+        m_agent = GetComponent<NavMeshAgent>();
+
+        if(m_agent != null)
+        {
+            if (Speed > 0f)
+                m_agent.speed = Speed;
+        }
+        else
+        {
+            Debug.LogError("Unable to find agent on Pig prefab");
+        }
     }
 
-    void Update ()
+    private void FixedUpdate ()
     {
+        m_agent.SetDestination(TargetTransform.position);
     }
 
     public void SetTargetPosition(Transform t)
     {
         TargetTransform = t;
+    }
+
+    private void OnTriggerEnter(Collider collider)
+    {
+        if (collider.isTrigger && collider.tag == TagConstants.HOUSE_TRIGGER_TAG)
+        {
+            collider.gameObject.GetComponentInParent<HouseController>().AddPigs(1);
+            Destroy(this.gameObject);
+        }
     }
 }
